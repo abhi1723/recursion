ea7970c3660aa8004703445658bf34dac5d41826
Abhishek Kumar <abhishekisme1996@gmail.com>
2019-04-10 02:30:39 +0530

    Search For the last occurance of A number in an array using Linear Search

--- FoundAtLast/main.cpp
+++ FoundAtLast/main.cpp
@@ -6,7 +6,7 @@ void input_array(lint* A,lint n){
     for(lint i=0;i<n;i++)
         cin>>A[i];
 }
-lint searchLastOccuranceOfTheElement(lint *A,lint s,lint e,lint elementToBeSearched,lint index){
+/*lint searchLastOccuranceOfTheElement(lint *A,lint s,lint e,lint elementToBeSearched,lint index){
     if(s>e) return -1;
     lint mid=(s+e)/2;
     if(A[mid]==elementToBeSearched) {index=mid;s=mid+1;}
@@ -16,6 +16,12 @@ lint searchLastOccuranceOfTheElement(lint *A,lint s,lint e,lint elementToBeSearc
     if(newIndex<index)
     return index;
     else return newIndex;
+}*/
+lint searchLastOccuranceOfElementLinearSearch(lint *A,int n,int elementToBeSearched){
+    if(n==-1)return -1;
+    if(A[n]==elementToBeSearched) return n;
+    int foundOrNotFound=searchLastOccuranceOfElementLinearSearch(A,n-1,elementToBeSearched);
+    return foundOrNotFound;
 }
 int main()
 {
@@ -26,7 +32,8 @@ int main()
     lint elementToBeSearched;
     cin>>elementToBeSearched;
     sort(A,A+n);
-    lint foundOrNotFound=searchLastOccuranceOfTheElement(A,0,n-1,elementToBeSearched,-1);
+    //lint foundOrNotFound=searchLastOccuranceOfTheElement(A,0,n-1,elementToBeSearched,-1);
+    lint foundOrNotFound=searchLastOccuranceOfElementLinearSearch(A,n-1,elementToBeSearched);
     cout<<foundOrNotFound;
     return 0;
 }
