e5390a12a75a299dfa97eae71774d62ebf4f70eb
Abhishek Kumar <abhishekisme1996@gmail.com>
2019-04-10 02:18:20 +0530

    Search For the last occurance of A number in an array using Binary Search

---
+++ FoundAtLast/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#define lint long long int
+using namespace std;
+void input_array(lint* A,lint n){
+    for(lint i=0;i<n;i++)
+        cin>>A[i];
+}
+lint searchLastOccuranceOfTheElement(lint *A,lint s,lint e,lint elementToBeSearched,lint index){
+    if(s>e) return -1;
+    lint mid=(s+e)/2;
+    if(A[mid]==elementToBeSearched) {index=mid;s=mid+1;}
+    if(A[mid]<elementToBeSearched) s=mid+1;
+    if(A[mid]>elementToBeSearched) e=mid-1;
+    lint newIndex=searchLastOccuranceOfTheElement(A,s,e,elementToBeSearched,index);
+    if(newIndex<index)
+    return index;
+    else return newIndex;
+}
+int main()
+{
+    lint n;
+    cin>>n;
+    lint A[n];
+    input_array(A,n);
+    lint elementToBeSearched;
+    cin>>elementToBeSearched;
+    sort(A,A+n);
+    lint foundOrNotFound=searchLastOccuranceOfTheElement(A,0,n-1,elementToBeSearched,-1);
+    cout<<foundOrNotFound;
+    return 0;
+}
