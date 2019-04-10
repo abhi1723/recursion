14a839a56e01871643213213802a8fffba8d7ff5
Abhishek Kumar <abhishekisme1996@gmail.com>
2019-04-10 17:06:35 +0530

    Take A string as input and between two consequtive characters eliminate either one of them if both of these characters are same

---
+++ RemoveDuplicates/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+string formatTheWord(string word,int n,int len){
+    if(n==-1) return "";
+    string formattedWord="";
+    formattedWord+=formatTheWord(word,n-1,len);
+    if(n==len) formattedWord+=word[n];
+    else {
+            string s(1, word[n]);
+            if(word[n]==word[n+1]) {}
+            else formattedWord+=word[n];
+    }
+    return formattedWord;
+}
+int main()
+{
+    string word,formattedWord;
+    cin>>word;
+    int len=word.length();
+    formattedWord=formatTheWord(word,len-1,len-1);
+    cout << formattedWord<< endl;
+    return 0;
+}
