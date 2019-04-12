263d56a243b09e37acef87979c9993fadf63c0d7
Abhishek Kumar <abhishekisme1996@gmail.com>
2019-04-12 21:24:35 +0530

    Generating all the strings that can be generated while dialing a number on a phone and printing all strings from an array which have any of those strings as substring

---
+++ PhoneKeypadAdvanced/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+using namespace std;
+void possibleStrings(string  numberInString,string possibleString){
+     string searchIn [] = {
+            "prateek", "sneha", "deepak", "arnav", "shikha", "palak",
+            "utkarsh", "divyam", "vidhi", "sparsh", "akku"
+    };
+    if(numberInString.length()==0)
+        {
+             for(int i=0;i<sizeof(searchIn)/sizeof(string);i++){
+                 size_t found = searchIn[i].find(possibleString);
+                 if (found != string::npos)
+                 cout << searchIn[i] << endl;
+             }
+             return;
+        }
+    string uri="",turi="";
+    uri+=numberInString[0];
+    for(int i=1;i<numberInString.length();i++)
+        turi+=numberInString[i];
+    stringstream geek(uri);
+    int n;
+    geek>>n;
+    int p=n%10;
+    if(p==0)
+        possibleStrings(turi,possibleString+" ");
+    if(p==1){
+        possibleStrings(turi,possibleString+".");
+        possibleStrings(turi,possibleString+"+");
+        possibleStrings(turi,possibleString+"@");
+        possibleStrings(turi,possibleString+"$");
+    }
+    if(p==2){
+        possibleStrings(turi,possibleString+"a");
+        possibleStrings(turi,possibleString+"b");
+        possibleStrings(turi,possibleString+"c");
+    }
+    if(p==3){
+        possibleStrings(turi,possibleString+"d");
+        possibleStrings(turi,possibleString+"e");
+        possibleStrings(turi,possibleString+"f");
+    }
+    if(p==4){
+        possibleStrings(turi,possibleString+"g");
+        possibleStrings(turi,possibleString+"h");
+        possibleStrings(turi,possibleString+"i");
+    }
+    if(p==5){
+        possibleStrings(turi,possibleString+"j");
+        possibleStrings(turi,possibleString+"k");
+        possibleStrings(turi,possibleString+"l");
+    }
+    if(p==6){
+        possibleStrings(turi,possibleString+"m");
+        possibleStrings(turi,possibleString+"n");
+        possibleStrings(turi,possibleString+"o");
+    }
+    if(p==7){
+        possibleStrings(turi,possibleString+"p");
+        possibleStrings(turi,possibleString+"q");
+        possibleStrings(turi,possibleString+"r");
+        possibleStrings(turi,possibleString+"s");
+    }
+    if(p==8){
+        possibleStrings(turi,possibleString+"t");
+        possibleStrings(turi,possibleString+"u");
+        possibleStrings(turi,possibleString+"v");
+    }
+    if(p==9){
+        possibleStrings(turi,possibleString+"w");
+        possibleStrings(turi,possibleString+"x");
+        possibleStrings(turi,possibleString+"y");
+        possibleStrings(turi,possibleString+"z");
+    }
+}
+int main()
+{
+    string numberInString;
+    cin>>numberInString;
+    stringstream geek(numberInString);
+    int n;
+    geek>>n;
+    possibleStrings(numberInString,"");
+    return 0;
+}
